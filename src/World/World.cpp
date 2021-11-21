8a57065c2ba97871350737251b038b0e5c6fccca
Adrian Kern <adrianmilokern@gmail.com>
2021-11-20 17:54:30 -0800

    chunk deletion bug fixed

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -54,7 +54,6 @@ void World::genPass()
 		delete[] heightMap;
 
 		m_Chunks.push_back(chunk);
-		//std::cout << "Chunk generated at : " << m_Chunks[m_Chunks.size() - 1]->getLocation().x << ", " << m_Chunks[m_Chunks.size() - 1]->getLocation().y << "\n";
 	}
 }
 
@@ -67,9 +66,9 @@ void World::destroyPass(Vector2i playerPos)
 	{
 		Vector2i chunkLoc{ m_Chunks[i]->getLocation() };
 
-		if (std::abs(chunkLoc.x - playerPos.x) > constants::renderDistance || std::abs(chunkLoc.y - playerPos.y) > constants::renderDistance)
+		//+ 1 becuase otherwise chunks that had just been generated got deleted
+		if (std::abs(chunkLoc.x - playerPos.x) > constants::renderDistance + 1 || std::abs(chunkLoc.y - playerPos.y) > constants::renderDistance + 1)
 		{
-			//std::cout << "Chunk deleted at: " << m_Chunks[i]->getLocation().x << ", " << m_Chunks[i]->getLocation().y << "\n";
 			delete m_Chunks[i];
 			m_Chunks[i] = nullptr;
 		}
@@ -99,7 +98,6 @@ void World::buildPass()
 
 		if (sectionPtr == g_ChunkCap)
 		{
-			//std::cout << "Mesh built at " << currentChunk->getLocation().x << ", " << currentChunk->getLocation().y << "\n";
 			m_Manager.getBuildQueue().pop_back();
 
 			currentChunk = nullptr;
